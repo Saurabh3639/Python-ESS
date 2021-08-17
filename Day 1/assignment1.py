8c038447770bcba1daa763f81e6ae71040df8164
Saurabh3639 <88394186+Saurabh3639@users.noreply.github.com>
2021-08-17 12:26:58 +0530

    Day 1 Assignment

---
+++ Day 1/assignment1.py
@@ -0,0 +1,9 @@
+'''
+Take two inputs and store in variable x and y, find x to the power of y.
+'''
+x = int(input("Enter the first value: "))
+y = int(input("Enter the second value: "))
+
+# x to the power of y
+ans = pow(x,y) 
+print(ans)
